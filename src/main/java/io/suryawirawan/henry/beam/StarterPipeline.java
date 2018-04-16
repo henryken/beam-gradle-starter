02e1ae205b178dc5c422378cf3cbc8d1141272c0
Henry Suryawirawan <hsuryawirawan@google.com>
2018-04-16 23:36:20 +0800

    Add starter pipeline which does nothing

---
+++ src/main/java/io/suryawirawan/henry/beam/StarterPipeline.java
@@ -0,0 +1,18 @@
+package io.suryawirawan.henry.beam;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+
+public class StarterPipeline {
+
+    public static void main(String[] args) {
+        PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
+        Pipeline pipeline = Pipeline.create(options);
+
+
+
+        pipeline.run();
+    }
+
+}
